9e21805b2b76da42b4272c83a3d561ac3a7150bf
JeongUkJae <jeongukjae@gmail.com>
2018-06-27 02:52:24 +0900

    로그인 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+import update from "immutability-helper";
+
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Paper from "@material-ui/core/Paper";
@@ -11,12 +13,13 @@ import { withStyles, WithStyles } from "@material-ui/core/styles";
 import MarginedContainer from "./components/MarginedContainer";
 import MenuAppBar from "./components/MenuAppBar";
 
-// import { login } from './lib/api';
+import { login } from "./lib/api";
 
 interface AppStates {
   user?: {};
-  userId: string;
-  password: string;
+  data: {
+    [key: string]: string;
+  };
 }
 
 const styles = {
@@ -33,9 +36,29 @@ type AppProps = WithStyles<keyof typeof styles>;
 
 class App extends React.Component<AppProps, AppStates> {
   state = {
-    password: "",
-    user: undefined,
-    userId: ""
+    data: {
+      password: "",
+      userId: ""
+    },
+    user: undefined
+  };
+
+  handleChange = (name: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      data: update(this.state.data, { [name]: { $set: e.target.value } })
+    });
+    e.preventDefault();
+  };
+
+  onLogin = () => {
+    login(this.state.data.userId, this.state.data.password).then(value => {
+      // tslint:disable-next-line:no-console
+      let cookie = value.data.JSESSIONID;
+
+      return cookie;
+    }).catch((reason) => {
+      alert(`로그인 실패 ${reason}`);
+    });
   };
 
   render() {
@@ -47,27 +70,35 @@ class App extends React.Component<AppProps, AppStates> {
 
         <MenuAppBar auth={true} />
         <Paper className={classes.root} elevation={4}>
-          <Typography variant="title">로그인</Typography>
           <MarginedContainer>
-            <TextField
-              label="아이디"
-              placeholder="아이디"
-              fullWidth={true}
-              margin="dense"
-            />
-            <TextField
-              label="비밀번호"
-              placeholder="비밀번호"
-              fullWidth={true}
-              margin="dense"
-            />
-            <Button
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              로그인
-            </Button>
+            <Typography variant="title">로그인</Typography>
+            <MarginedContainer>
+              <TextField
+                value={this.state.data.userId}
+                label="아이디"
+                placeholder="아이디"
+                fullWidth={true}
+                margin="dense"
+                onChange={this.handleChange("userId")}
+              />
+              <TextField
+                value={this.state.data.password}
+                label="비밀번호"
+                placeholder="비밀번호"
+                fullWidth={true}
+                margin="dense"
+                type="password"
+                onChange={this.handleChange("password")}
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+                onClick={this.onLogin}
+              >
+                로그인
+              </Button>
+            </MarginedContainer>
           </MarginedContainer>
         </Paper>
       </React.Fragment>
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,19 +3,14 @@ import axios from "axios";
 const login = (userId: string, password: string) => {
   return axios
     .post(
-      "http://portal.uos.ac.kr/user/uosMobileLoginProcess.face",
+      "http://localhost:5000/login",
       {
         password,
         userId
-      },
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        }
       }
     )
     .then(value => {
-      alert(value);
+      return value;
     });
 };
 
