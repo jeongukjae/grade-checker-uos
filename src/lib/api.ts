f08655e491c0b8f26b29a2d8f807cfcde4aa1cc9
JeongUkJae <JeongUkJae@gmail.com>
2018-12-12 21:35:37 +0900

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,17 +1,37 @@
 import axios from "axios";
+import { Dispatch } from "redux";
 
-const login = (userId: string, password: string) => {
-  return axios
-    .post(`${process.env.SERVER}/login`, {
-      password,
-      userId
-    })
-    .then(value => {
-      return value;
-    });
+import { failLogin, loginComplete } from "../store/modules/auth";
+
+export const login = (userId: string, password: string) => {
+  return (dispatch: Dispatch) => {
+    axios
+      .post(
+        `https://wise.uos.ac.kr/uosdoc/com.StuLogin.serv`,
+        {},
+        {
+          params: {
+            _COMMAND_: "LOGIN",
+            login_div_1_nm: "%C7%D0%BB%FD",
+            strIpAddr: "0.0.0.0",
+            strLoginId: userId,
+            strLoginPw: password,
+            strMacAddr: "0.0.0.0",
+            strTarget: "MAIN"
+          }
+        }
+      )
+      .then(value => {
+        dispatch(loginComplete());
+        return value;
+      })
+      .catch(reason => {
+        dispatch(failLogin(reason));
+      });
+  };
 };
 
-const grade = (cookie: string, userNo: string) => {
+export const grade = (cookie: string, userNo: string) => {
   return axios
     .post(`${process.env.SERVER}/grade`, {
       cookie,
@@ -21,6 +41,3 @@ const grade = (cookie: string, userNo: string) => {
       return value.data;
     });
 };
-
-export { login };
-export { grade };
