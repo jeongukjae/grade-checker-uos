5c6430b68363360bf9198d07c80941c7272637b0
JeongUkJae <JeongUkJae@gmail.com>
2018-12-14 14:37:23 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
 import * as React from "react";
 
+import { Switch, Route } from "react-router-dom";
+
 import { withStyles, WithStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+import Grades from "./routes/grades";
+import MenuAppBar from "./components/MenuAppBar";
 
 interface IAppStates {}
 
-const styles = {};
+const styles = {
+  root: {
+    margin: 20,
+    padding: "16px 10px"
+  }
+};
 
 type IAppProps = WithStyles<keyof typeof styles>;
 
 class App extends React.Component<IAppProps, IAppStates> {
   public render() {
-    return <div />;
+    const { classes } = this.props;
+    return (
+      <>
+        <CssBaseline />
+        <MenuAppBar auth={false} />
+        <Paper className={classes.root} elevation={2}>
+          <Switch>
+            <Route exact path="/" component={Grades} />
+          </Switch>
+        </Paper>
+      </>
+    );
   }
 }
 
